42389f36ce9b31c0eaf8e928223fcc348104228a
Abutalif Khan <talifkhan1@hotmail.com>
2024-08-19 11:28:42 +0530

    add - email verification func

--- src/utils/nodemailer.js
+++ src/utils/nodemailer.js
@@ -1,5 +1,6 @@
 import nodemailer from "nodemailer";
 import { ApiError } from "./ApiError.js";
+import emailVerificationTemplate from "../templates/emailVerificationTemplate.js";
 export const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 587,
@@ -10,12 +11,23 @@ export const transporter = nodemailer.createTransport({
     },
 });
 
-export const sendMail = async (mailOptions) => {
-    await transporter.sendMail(mailOptions, (error, info) => {
+export const sendMail = (email, fullName, confirmationLink) => {
+    const template = emailVerificationTemplate(
+        fullName.split(" "),
+        confirmationLink
+    );
+    const mailOptions = {
+        from: process.env.EMAIL_USER || "talifkh2003@gmail.com",
+        to: email || "abutalifkh1@gmail.com",
+        subject: "Confirm your registration",
+        priority: "high",
+        html: template,
+    };
+    transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
             console.error("Error sending email:", error);
             // throw new ApiError(500, "Failed to send confirmation email");
-            return
+            return;
         }
         console.log("Confirmation email sent:", info.response);
     });

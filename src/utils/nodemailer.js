8622f7cc33247bdaab6b752754de519ba1dc5d22
Abutalif Khan <talifkhan1@hotmail.com>
2024-08-20 22:33:17 +0530

    update - nodemailer config

--- src/utils/nodemailer.js
+++ src/utils/nodemailer.js
@@ -1,24 +1,22 @@
 import nodemailer from "nodemailer";
-import { ApiError } from "./ApiError.js";
 import emailVerificationTemplate from "../templates/emailVerificationTemplate.js";
 export const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false, // Use `true` for port 465, `false` for all other ports
+    host: process.env.MAIL_HOST,
+    port: 465,
+    secure: true, // Use `true` for port 465, `false` for all other ports
     auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS,
     },
 });
-
 export const sendMail = (email, fullName, confirmationLink) => {
     const template = emailVerificationTemplate(
         fullName.split(" "),
         confirmationLink
     );
     const mailOptions = {
-        from: process.env.EMAIL_USER || "talifkh2003@gmail.com",
-        to: email || "abutalifkh1@gmail.com",
+        from: process.env.EMAIL_USER,
+        to: email,
         subject: "Confirm your registration",
         priority: "high",
         html: template,

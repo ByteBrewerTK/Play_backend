927b2c35213035241a6b9b3a79377ca7d6cbe87f
Abutalif Khan <talifkhan1@hotmail.com>
2024-09-01 23:05:46 +0530

    format - code

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -5,7 +5,7 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const createPlaylist = asyncHandler(async (req, res) => {
-    const { name, description="" } = req.body;
+    const { name, description = "" } = req.body;
     const { privacyType } = req.params;
 
     if (!name) {

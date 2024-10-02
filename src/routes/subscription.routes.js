199f431e53eafad50957ecca677c853c8c9d5b09
Abutalif Khan <talifkhan1@hotmail.com>
2024-10-02 23:40:38 +0530

    add - getSubscriptions video route

--- src/routes/subscription.routes.js
+++ src/routes/subscription.routes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { verifyJWT } from "../middleware/auth.middleware.js";
 import {
     getSubscribedChannels,
+    getSubscriptionVideos,
     getUserChannelSubscribers,
     toggleSubscription,
 } from "../controllers/subscription.controller.js";
@@ -15,5 +16,7 @@ router
     .route("/channel/:channelId")
     .patch(toggleSubscription)
     .get(getUserChannelSubscribers);
+
+router.route("/videos").get(getSubscriptionVideos);
 
 export default router;

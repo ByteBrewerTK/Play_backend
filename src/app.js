ea0efee2d64e08aa949b358f03169cc60a8f3948
Abutalif Khan <talifkhan1@hotmail.com>
2024-11-22 23:47:57 +0530

    update - chat endpoint

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ import likeRouter from "./routes/like.routes.js";
 import commentRouter from "./routes/comment.routes.js";
 import dashboardRouter from "./routes/dashboard.routes.js";
 import settingRouter from "./routes/setting.routes.js";
+import chatRoute from "./routes/chat.routes.js";
 
 const app = express();
 
@@ -69,5 +70,6 @@ app.use("/api/v1/like", likeRouter);
 app.use("/api/v1/comment", commentRouter);
 app.use("/api/v1/dashboard", dashboardRouter);
 app.use("/api/v1/settings", settingRouter);
+app.use("/api/v1/chat", chatRoute);
 
 export { app };

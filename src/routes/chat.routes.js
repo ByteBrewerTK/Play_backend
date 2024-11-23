1ed905021fa6a7c9ab46e600fb80e5985037e48a
Abutalif Khan <talifkhan1@hotmail.com>
2024-11-23 15:42:36 +0530

    update - new routes

--- src/routes/chat.routes.js
+++ src/routes/chat.routes.js
@@ -2,8 +2,12 @@ import { Router } from "express";
 import { verifyJWT } from "../middleware/auth.middleware.js";
 import {
     accessChat,
+    addToGroup,
+    clearGroupChats,
     createGroup,
+    deleteGroup,
     fetchChat,
+    removeFromGroup,
     renameGroup,
 } from "../controllers/chat.controller.js";
 
@@ -14,9 +18,9 @@ router.use(verifyJWT);
 router.route("/").get(fetchChat).post(accessChat);
 router.route("/group").post(createGroup);
 router.route("/group/rename").patch(renameGroup);
-// router.route("/group/add").put(addToGroup);
-// router.route("/group/remove").put(removeFromGroup);
-// router.route("/group/delete").delete(deleteGroup);
-// router.route("/group/clear").put(clearGroupChats)
+router.route("/group/add").put(addToGroup);
+router.route("/group/remove").put(removeFromGroup);
+router.route("/group/clear").put(clearGroupChats)
+router.route("/group/delete").delete(deleteGroup);
 
 export default router;

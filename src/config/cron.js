8817ac919abe1c7cfcdfe203ae65b1055903ab8c
Abutalif Khan <talifkhan1@hotmail.com>
2024-11-25 21:06:07 +0530

    update - schedule keep alive job

--- src/config/cron.js
+++ src/config/cron.js
@@ -1,9 +1,11 @@
 import cron from "node-cron";
 
 import deleteExpiredOtps from "../cron/deleteExpiredOtps.js";
+import keepAlive from "../cron/keepAlive.js";
 
 const initCronJobs = () => {
     cron.schedule("* * * * *", deleteExpiredOtps); // Runs every minute
+    cron.schedule("*/10 * * * *", keepAlive); //Runs every 10 minute
     console.log("Cron jobs initialized");
 };
 

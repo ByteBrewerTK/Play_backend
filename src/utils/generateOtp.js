a6c1d031fffd9fe7ab64d472c1ba132500b7f7bb
Abutalif Khan <talifkhan1@hotmail.com>
2024-12-02 23:50:23 +0530

    update - generate otp to alpha numeric

--- src/utils/generateOtp.js
+++ src/utils/generateOtp.js
@@ -1,6 +1,11 @@
-const generateOtp = () => {
-    const otp = Math.floor(100000 + Math.random() * 900000);
+const generateOtp = (length = 6) => {
+    const chars =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    let otp = "";
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * chars.length);
+        otp += chars[randomIndex];
+    }
     return otp;
 };
-
 export default generateOtp;

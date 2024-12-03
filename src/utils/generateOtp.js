cb2a7072f0afd9b93ce9b8127cb34bc5138574c0
Abutalif Khan <talifkhan1@hotmail.com>
2024-12-03 20:23:26 +0530

    update - otp combination

--- src/utils/generateOtp.js
+++ src/utils/generateOtp.js
@@ -1,6 +1,6 @@
 const generateOtp = (length = 6) => {
     const chars =
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        "A0@B1#C2$D3%E4&F5@G6#H7$I8%J9&K0L1@M2#N3$O4%P5&Q6@R7#S8$T9%U0&V@W#X$Y%Z&";
     let otp = "";
     for (let i = 0; i < length; i++) {
         const randomIndex = Math.floor(Math.random() * chars.length);
